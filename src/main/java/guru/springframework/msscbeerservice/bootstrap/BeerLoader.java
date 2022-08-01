8aa207f8bf420c01bfd7cef4be8e770588244eb7
Popescu Robert Ionut <robertpopescu1903@gmail.com>
2022-08-02 00:27:26 +0300

    Created Beer Bootstrap Data

---
+++ src/main/java/guru/springframework/msscbeerservice/bootstrap/BeerLoader.java
@@ -0,0 +1,51 @@
+package guru.springframework.msscbeerservice.bootstrap;
+
+import guru.springframework.msscbeerservice.domain.Beer;
+import guru.springframework.msscbeerservice.repositories.BeerRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
+
+/**
+ * Class that will be executed by the Spring Context every time it runs.
+ */
+@Component
+public class BeerLoader implements CommandLineRunner {
+
+    private final BeerRepository beerRepository;
+
+    public BeerLoader(BeerRepository beerRepository) {
+        this.beerRepository = beerRepository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        loadBeerObjects();
+    }
+
+    /**
+     * If the repository is empty and has no objects, then some objects will be injected automatically.
+     * Otherwise, this method will be skipped.
+     */
+    private void loadBeerObjects() {
+        if(beerRepository.count() == 0) {
+            beerRepository.save(Beer.builder()
+                    .beerName("Mango Bobs")
+                    .beerStyle("IPA")
+                    .quantityToBrew(200)
+                    .minOnHand(12)
+                    .upc(33701000001L)
+                    .price(new BigDecimal("12.95"))
+                    .build());
+            beerRepository.save(Beer.builder()
+                    .beerName("Galaxy Cat")
+                    .beerStyle("PALE_ALE")
+                    .quantityToBrew(200)
+                    .minOnHand(12)
+                    .upc(33701000002L)
+                    .price(new BigDecimal("11.95"))
+                    .build());
+        }
+    }
+}
