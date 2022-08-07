74c7edc920772f020537f5145dcd26f5ce8ae599
Popescu Robert Ionut <robertpopescu1903@gmail.com>
2022-08-07 18:55:00 +0300

    Documented Validation Constraints

--- src/main/java/guru/springframework/msscbeerservice/web/model/BeerDto.java
+++ src/main/java/guru/springframework/msscbeerservice/web/model/BeerDto.java
@@ -5,10 +5,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Null;
-import javax.validation.constraints.Positive;
+import javax.validation.constraints.*;
 import java.math.BigDecimal;
 import java.time.OffsetDateTime;
 import java.util.UUID;
@@ -29,6 +26,7 @@ public class BeerDto {
     private OffsetDateTime lastModifiedDate;
 
     @NotBlank
+    @Size(min = 3, max = 100)
     private String beerName;
 
     @NotNull
@@ -42,5 +40,6 @@ public class BeerDto {
     @Positive
     private BigDecimal price;
 
+    @Positive
     private Integer quantityOnHand;
 }
