3b5587275432638483b3ac2e3ae3a6aa3e9ace14
Popescu Robert Ionut <robertpopescu1903@gmail.com>
2022-08-07 17:12:38 +0300

    Documented Query parameter (example)

--- src/test/java/guru/springframework/msscbeerservice/web/controller/BeerControllerTest.java
+++ src/test/java/guru/springframework/msscbeerservice/web/controller/BeerControllerTest.java
@@ -18,8 +18,7 @@ import java.util.UUID;
 
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.*;
-import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
-import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
+import static org.springframework.restdocs.request.RequestDocumentation.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @AutoConfigureRestDocs
@@ -36,10 +35,14 @@ class BeerControllerTest {
 
     @Test
     void getBeerById() throws Exception{
-        mockMvc.perform(get("/api/v1/beer/{beerId}", UUID.randomUUID().toString()).accept(MediaType.APPLICATION_JSON))
+        mockMvc.perform(get("/api/v1/beer/{beerId}", UUID.randomUUID().toString())
+                        .param("iscold", "yes")
+                        .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andDo(document("v1/beer", pathParameters(
                         parameterWithName("beerId").description("UUID of desired beer to get.")
+                ), requestParameters(
+                        parameterWithName("iscold").description("Is Beer Cold Query param")
                 )));
     }
 
