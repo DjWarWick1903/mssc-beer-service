ce0193187ed733dbb8e201bbb2f2ca135a3f943f
Popescu Robert Ionut <robertpopescu1903@gmail.com>
2022-08-21 21:15:28 +0300

    Created Beer Event Objects

---
+++ src/main/java/guru/springframework/msscbeerservice/events/BeerEvent.java
@@ -0,0 +1,18 @@
+package guru.springframework.msscbeerservice.events;
+
+import guru.springframework.msscbeerservice.web.model.BeerDto;
+import lombok.Builder;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+import java.io.Serializable;
+
+@Data
+@RequiredArgsConstructor
+@Builder
+public class BeerEvent implements Serializable {
+
+    static final long serialVersionUID = 123l;
+
+    private final BeerDto beerDto;
+}
