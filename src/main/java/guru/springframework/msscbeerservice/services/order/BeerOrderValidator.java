ebf1b17623c42dcfaa7ae282d6a3a9eebbb4ee74
Popescu Robert Ionut <robertpopescu1903@gmail.com>
2022-09-10 10:57:25 +0300

    Handle validate order request

---
+++ src/main/java/guru/springframework/msscbeerservice/services/order/BeerOrderValidator.java
@@ -0,0 +1,28 @@
+package guru.springframework.msscbeerservice.services.order;
+
+import guru.sfg.brewery.model.BeerOrderDto;
+import guru.springframework.msscbeerservice.repositories.BeerRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class BeerOrderValidator {
+    private final BeerRepository beerRepository;
+
+    public Boolean validateOrder(BeerOrderDto beerOrder) {
+        AtomicInteger beersNotFound = new AtomicInteger();
+
+        beerOrder.getBeerOrderLines().forEach(orderLine -> {
+            if(beerRepository.findByUpc(orderLine.getUpc()) == null) {
+                beersNotFound.incrementAndGet();
+            }
+        });
+
+        return beersNotFound.get() == 0;
+    }
+}
